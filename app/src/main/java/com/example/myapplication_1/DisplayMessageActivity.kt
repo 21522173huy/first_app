8cd6078cea309e33fd217701a110856d623bf62d
foo <foo@localhost>
2022-04-16 19:09:24 +0000

    add viewModel

--- app/src/main/java/com/example/myapplication_1/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/myapplication_1/DisplayMessageActivity.kt
@@ -3,8 +3,16 @@ package com.example.myapplication_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+
+class MyViewModel: ViewModel() {
+    val isReady = true
+    val x = 7
+}
 
 class DisplayMessageActivity : AppCompatActivity() {
+    val myViewModel by viewModels<MyViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
